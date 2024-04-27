c0f707b6eca3708ccf6414db029b99f3b330f8a6
suricato <gabrielmelobatista@hotmail.com>
2024-04-27 18:34:59 -0300

    Adicionado Cryptografia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { UsuarioControllerService } from './api/services/usuario-controller.service'
-import { HttpClientModule, HttpClient } from "@angular/common/http";
+import crypto from 'crypto-js';
+import {UsuarioDto} from './api/models'
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,10 @@ import { HttpClientModule, HttpClient } from "@angular/common/http";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  httpClient = inject(HttpClient)
   public mostrarCadastro = true;
   formGroup!: FormGroup;
   mensagem: string = '';
+  salt: number = 256789;
 
   constructor(
     private fb: FormBuilder,
@@ -23,35 +23,66 @@ export class AppComponent implements OnInit {
       this.createForm();
     }
 
-  createForm() {
+  createForm() : void {
     this.formGroup = this.fb.group({
       email: ['', Validators.required],
       senha: ['', Validators.required],
     });
   }
 
-    ngOnInit() { }
+    ngOnInit(): void { }
 
-  toggleCadastroLogin() {
+  toggleCadastroLogin() : void {
     this.mostrarCadastro = !this.mostrarCadastro;
-    this.formGroup.get('nome')?.updateValueAndValidity(); // Update validation
   }
 
-  onSubmit() {
-	  console.log("Foi :)");
+  // Função que Criptografa uma string, retornando uma hash segura
+  encriptData(data: string): string {
+    // Biblioteca Crypto e padrão do node e não precisa ser instalada.
+    return crypto.SHA256(data).toString();
+  }
+
+  // Captura o movimento do mouse, gerando salt aleatorios a cada movimento do mouse
+  @HostListener('document:mousemove', ['$event'])
+  async onmousemove(event: MouseEvent) {
+    let mouseEntropy = event.clientX + event.clientY;
+    let randomSalt = await this.generateRandomSalt();
+    this.salt = mouseEntropy ^ randomSalt;
+  }
+
+  // Função que gera salt aleatorio
+  async generateRandomSalt() {
+    if(window.crypto && window.crypto.getRandomValues) {
+      let array = new Uint32Array(1);
+      window.crypto.getRandomValues(array);
+      return array[0];
+    }
+    else {
+      // caso a biblioteca assima falhe, usa a biblioteca padrão, não recomendado mas e para evitar do programa de falhar
+      return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    }
+  }
+
+  onSubmit() : void {
+    // tecnica para evitar que o formulario mostre o hash para o usuario, e fique apenas com as informações de login na tela.
+    const usuario: UsuarioDto = {};
+
+    usuario.email = this.encriptData(this.formGroup.get('email')?.value);
+    usuario.senha = this.encriptData(this.formGroup.get('senha')?.value);
+
     if (this.mostrarCadastro) {
-          this.cadastroService.cadastro({body: this.formGroup.value})
-            .subscribe(response => {
-              this.mensagem = 'Cadastro realizado com sucesso!';
+          this.cadastroService.cadastro({body: usuario})
+            .subscribe((response) => {
+              this.mensagem = 'Cadastro realizado com sucesso! Salt:' + this.salt.toString();
             }, error => {
               this.mensagem = 'Erro ao realizar cadastro.';
               console.error(error);
             });
         } else {
-          this.loginService.login({body: this.formGroup.value})
-            .subscribe(response => {
+          this.loginService.login({body: usuario})
+            .subscribe((response) => {
               // Process login success (e.g., redirect)
-              console.log('Login realizado com sucesso!');
+              this.mensagem = 'Login realizado com sucesso!';
             }, error => {
               this.mensagem = 'Erro ao realizar login.';
               console.error(error);
